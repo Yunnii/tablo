4a30817fd79dd7d17c181c8969b721719a49cf63
Luba Amiraslanova <yunnii@yandex-team.ru>
2015-08-09 12:49:42 +0500

    путь до картинок

--- static/blocks/Logo/logo.jsx
+++ static/blocks/Logo/logo.jsx
@@ -3,7 +3,7 @@ var b = require('b_').with('logo');
 var Logo = React.createClass({
     render: function () {
         return (
-            <img src={'/tablo/static/deploy//i/' + this.props.name + '.png'}
+            <img src={'/tablo/static/deploy/i/' + this.props.name + '.png'}
                 alt={this.props.name} className={b({type: this.props.type})}/>
         );
     }
